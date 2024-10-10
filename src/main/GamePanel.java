06ca76a74aeb32e330000ddff780ba09e2810379
HheZi <paveltseva@gmail.com>
2024-10-10 20:28:58 +0300

    video 5

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -8,6 +8,7 @@ import java.awt.Graphics2D;
 import javax.swing.JPanel;
 
 import entity.Player;
+import tile.TileManager;
 
 public class GamePanel extends JPanel implements Runnable{
 	
@@ -16,22 +17,18 @@ public class GamePanel extends JPanel implements Runnable{
 	final int scale = 3;
 	
 	public final int tileSize = originalTileSize * scale; //48x48 tile 
-	final int maxScreenCol = 16;
-	final int maxScreenRow = 12;
-	final int screenWidth = tileSize * maxScreenCol; // 768 pix
-	final int screenHeight = tileSize * maxScreenRow; // 576 pix
+	public final int maxScreenCol = 16;
+	public final int maxScreenRow = 12;
+	public final int screenWidth = tileSize * maxScreenCol; // 768 pix
+	public final int screenHeight = tileSize * maxScreenRow; // 576 pix
 	
 	int FPS = 60;
 	
+	
+	TileManager tileM = new TileManager(this);
 	KeyHandler keyH = new KeyHandler();
 	Thread gameThread;
 	Player player = new Player(this,keyH);
-	
-	//Set plater`s default position
-	int playerX = 100;
-	int playerY = 100;
-	int playerSpeed = 4;
-	
 	
 	public GamePanel() {
 		
@@ -89,6 +86,8 @@ public class GamePanel extends JPanel implements Runnable{
     	super.paintComponent(g);
     	
     	Graphics2D g2 = (Graphics2D)g;
+    	
+    	tileM.draw(g2);
     	
     	player.draw(g2);
     	
